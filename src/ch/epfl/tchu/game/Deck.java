52417b0fa8eecf8a6f5e28fc476b794b2a9a4885
youssef amine <youssef.amine@epfl.ch>
2021-03-11 23:59:38 +0100

    maj

--- src/ch/epfl/tchu/game/Deck.java
+++ src/ch/epfl/tchu/game/Deck.java
@@ -15,9 +15,7 @@ public final class Deck<C extends Comparable<C>> {
     private final List<C> cards;
 
 
-    public List<C> getCards(){
-        return this.cards;
-    }
+
 
     /**
      * private constructor of Deck
@@ -105,6 +103,7 @@ public final class Deck<C extends Comparable<C>> {
      */
     public Deck<C> withoutTopCards(int count) {
         Preconditions.checkArgument(count >= 0 && count <= size());
+        List<C> c = new LinkedList<>(cards);
         Deck<C> deck = new Deck<>(cards);
         for(int i = 0 ; i <count ; ++i){
             deck = deck.withoutTopCard();
